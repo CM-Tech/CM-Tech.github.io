bff0989ee46a86cb16a55aee0495fe713e296aaa
Cole Kissane <coler706@gmail.com>
2020-04-02 19:47:04 -0700

    Thumbnail support

--- js/index.js
+++ js/index.js
@@ -32,14 +32,15 @@ function getRepos() {
     req.open('GET', 'https://api.github.com/users/cm-tech/repos?per_page=1000', false);
     req.send(null);
     var repos = eval(req.responseText);
+    console.log(repos);
     repos.forEach(function(e) {
         var a = document.createElement("div");
         a.className = "column is-12-mobile is-half-tablet is-one-third-desktop";
-        //<img class="project-thumb" src="http://api.page2images.com/directlink?p2i_url=http://cm-tech.github.io/${e.name}&p2i_device=6&p2i_screen=1024x600&p2i_size=512x300&p2i_wait=5&p2i_key=4bc9911353a20554" />
+
 
         a.innerHTML = `
-        <figure class="image is-4by3">
-            
+        <figure class="image is-2by1">
+        <img class="project-thumb" src="https://raw.githubusercontent.com/CM-Tech/${e.name.toLowerCase()}/master/README.png" onerror="this.style.display='none';this.parentElement.classList.add('no-image')"/>
           
             <figcaption>
                 <h1 class="title is-size-5 is-size-4-widescreen">${e.name}</h1><a class="button is-warning is-outlined is-rounded" href="${"http://cm-tech.github.io/" + e.name}" target="_blank"><span>Visit Website</span><span class="icon"><i class="fas fa-angle-right"></i><!-- <i class="fas fa-angle-right"></i> --></span></a>
