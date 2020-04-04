5ebc39ff09bff6bff4522ca098ea7d10d59b4125
modderme123 <modderme123@gmail.com>
2020-04-04 12:21:45 -0700

    more changes

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
+import "zenscroll";
+import { regl } from "./canvas";
+import * as config from "./config";
+import { fullscreen, update, display, drawLogo, createSplat } from "./shaders";
+
 // When the user scrolls the page, execute myFunction
 window.onscroll = navStick;
 
@@ -57,3 +62,34 @@ function getRepos() {
   });
 }
 getRepos();
+
+regl.frame(() => {
+  fullscreen(() => {
+    if (window.scrollY < window.innerHeight / 2) drawLogo(1.0 - config.DENSITY_DISSIPATION);
+    if (pointer.moved) {
+      createSplat(pointer.x, pointer.y, pointer.dx, pointer.dy, pointer.color, config.SPLAT_RADIUS);
+      pointer.moved = false;
+    }
+    update(config);
+    display();
+  });
+});
+
+let pointer = {
+  x: 0,
+  y: 0,
+  dx: 0,
+  dy: 0,
+  moved: false,
+  color: [0.5, 0.66, 1],
+};
+document.addEventListener("mousemove", (e) => {
+  pointer.moved = true;
+  pointer.dx = (e.clientX - pointer.x) * 10;
+  pointer.dy = (e.clientY - pointer.y) * 10;
+  pointer.x = e.clientX;
+  pointer.y = e.clientY;
+});
+document.addEventListener("mousedown", () => {
+  pointer.color = [Math.random() + 0.2, Math.random() + 0.2, Math.random() + 0.2];
+});
